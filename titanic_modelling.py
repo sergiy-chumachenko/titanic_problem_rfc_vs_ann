6f4447d17d11b97e72378bce998ebc9b9baefb7e
sergiy-chumachenko <chumachenko.sergiy@gmail.com>
2018-04-10 11:09:33 +0300

    [FIRST COMMIT] Repository initialization

---
+++ titanic_modelling.py
@@ -0,0 +1,17 @@
+# Random Forest vs Artificial Neural Network(ANN)
+
+# Installing Theano
+# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+
+# Installing Tensorflow
+# Install Tensorflow from the website: https://www.tensorflow.org/versions/r0.12/get_started/os_setup.html
+
+# Installing Keras
+# pip install --upgrade keras
+
+# Part 1 - Data Preprocessing
+
+# Importing the libraries
+import pandas as pd
+
+
